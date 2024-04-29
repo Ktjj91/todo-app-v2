6219a14bde141021f9470902682f516675be2874
Ktjj91 <kingsleythomas33@gmail.com>
2024-04-29 23:27:02 +0200

    new file

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,7 @@
-import {db} from "@/lib/kysely";
-import {seed} from "@/lib/generate-table-Tasks";
-export async  function  task() {
-    let tasks;
-    try {
-          tasks = db.selectFrom("tasks").selectAll().execute();
-          return tasks;
+import {task} from "@/lib/tasks";
 
-    } catch (e:any) {
-        if (e.message === `relation "tasks" does not exist`) {
-            console.log(
-                'Tasks does not exist, creating and seeding it with dummy data now...'
-            )
-            await  seed();
-            tasks = db.selectFrom("tasks").selectAll().execute();
-            return tasks;
-
-        } else {
-            throw new e
-        }
-    }
-}
 export default async function Home() {
+    const data = await task();
   return (
     <main className=" min-h-screen min-w-screen  ">
       <h1 className="text-5xl text-center">Todo list</h1>
